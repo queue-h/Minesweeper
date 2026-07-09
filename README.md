13fd6527c98d362ecf82f7cff1483624c9a01a5f
Quinn Helfand <qhelfand@gmail.com>
2026-07-09 18:33:18 -0400

    Fiddled with the Tile class, but realized I need some major refactoring and will branch from this commit.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Minesweeper
+
+Okay, here's how this should work:
+Tile.py - Tile class holds x, y, and status information
+Board.py - Board class creates and holds a matrix of Tiles
+Game.py - Probably doesn't need to be an object (take that, Java) should hold the pygame code and manipulate the Board class.
--- frontend.py
+++ frontend.py
@@ -1,27 +1,70 @@
+import enum
 import sys
 import pygame
 from pygame.locals import *
 from Player import Player
 
+class Tile:
+    # tile size and buffer might need to be passed in to account for changing window sizes
+    # but that's a later problem
+    tile_size = 40
+    buffer = 10
+
+    # every tile must have an x and y based on board location, x and y graphical location (independently calculated),
+    # size (global for now), buffer (global for now), and a status (-1 = tile is bomb, [0, 8] = num of surrounding bombs)
+    def __init__(self, x, y, board):
+        self.board_x = x
+        self.board_y = y
+        self.status = board[x, y]
+        self.screen_x, self.screen_y = self.get_screen_coord()
+
+    def get_rect(self):
+        return pygame.Rect(self.screen_x, self.screen_y, self.tile_size, self.tile_size)
+
+    def get_screen_coord(self):
+        return (self.board_x * self.tile_size) + self.buffer, (self.board_y * self.tile_size) + self.buffer
+
 class Game:
+    # i want to add ability to set board size, but i don't want to fuck with anything else right now
     def __init__(self, width, height):
+        # set up pygame
         pygame.init()
         self.fps = 60
         self.fpsClock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((width, height))
         # everything must go through self.player
         self.player = Player()
+        self.board = self.player._board
 
     def create_tiles(self):
-        num_tiles = self.player.board_size # var name is misleading, this is the number of tiles per side
-        tile_size = 40
-        buffer = 10
+        # i think i will probably need a matrix of tiles
+        # i might want to refactor the gameboard class
+        # shit
 
-        for r in range(buffer, (num_tiles * tile_size) + buffer, tile_size + buffer):
-            for c in range(buffer, (num_tiles * tile_size) + buffer, tile_size + buffer):
-                rect = pygame.Rect(r, c, tile_size, tile_size)
-                pygame.draw.rect(self.screen, 200, rect)
 
+        # num_tiles = self.player.board_size # var name can be misleading, this is the number of tiles per side
+        #
+        # for r in range(buffer, (num_tiles * tile_size) + buffer, tile_size + buffer):
+        #     for c in range(buffer, (num_tiles * tile_size) + buffer, tile_size + buffer):
+        #         rect = pygame.Rect(r, c, tile_size, tile_size)
+        #         pygame.draw.rect(self.screen, 200, rect)
+
+        button = pygame.Rect(5, 5, 20, 20) # this is a trial button, to see if i can do it with one before i try 100
+        b = pygame.draw.rect(self.screen, (255, 255, 255), button)
+        pygame.display.flip() # i don't know what this does
+
+        # loop to check for mouse action and its position
+        # figure out how to check if the mouse has clicked on a button--this is going to be
+        # a huge pain in the ass for no reason
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # if mouse is pressed get position of cursor
+                    pos = pygame.mouse.get_pos()
+                    ## check if cursor is on button
+                    if b.collidepoint(pos):
+                        ## exit ##
+                        return
 
     def play_game(self):
         # Game loop.
@@ -53,7 +96,7 @@ class Game:
 
 def main():
     game = Game(640, 480)
-    game.play_game()
+    game.create_tiles()
 
 if __name__ == '__main__':
     main()
